cfe258f770a3ecf9a18743f52a18dc67069e552c
ThisCatLikesCrypto <105151352+ThisCatLikesCrypto@users.noreply.github.com>
2024-03-04 11:28:37 +0000

    did nothing in the end
    ignore this commit

--- script.js
+++ script.js
@@ -17,5 +17,5 @@ function openTab(evt, tabName) {
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
-   }
-   
+  }
+  
