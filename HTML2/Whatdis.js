088acffd4cbc869640767e3d3db01c980b00fe72
thebookcheese <160770461+thebookcheese@users.noreply.github.com>
2024-03-08 08:27:58 +0000

    Update Whatdis.js

--- HTML2/Whatdis.js
+++ HTML2/Whatdis.js
@@ -31,4 +31,20 @@ function openTab(evt, tabName) {
     document.getElementById(tabName).style.display = "block";
     evt.currentTarget.className += " active";
   }
-  
+
+function myMove() {
+  let id = null;
+  const elem = document.getElementById("animate");
+  let pos = 0;
+  clearInterval(id);
+  id = setInterval(frame, 5);
+  function frame() {
+    if (pos == 350) {
+      clearInterval(id);
+    } else {
+      pos++;
+      elem.style.top = pos + 'px';
+      elem.style.left = pos + 'px';
+    }
+  }
+}
