82df141b8d0f70f8c979bca076246d6fcee2b533
Wilbur Williams <105151352+ThisCatLikesCrypto@users.noreply.github.com>
2024-03-14 20:29:52 +0000

    Update weard.js - cokie not cookie bing chat

--- games/cokie/weard.js
+++ games/cokie/weard.js
@@ -23,7 +23,7 @@ function alwaysOn100() {
   cokcl += 0.05 * cursorAmount;
   cokcl += 0.1 * procursorAmount;
   document.getElementById("counter").innerHTML = Math.round(cokcl);
-  document.title = Math.round(cokcl) + " cookies - Cookie Clicker";
+  document.title = Math.round(cokcl) + " cokies - Cokie Cliker";
 }
 
 function cookieClicked() {
