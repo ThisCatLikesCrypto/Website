81098b1febb1cf4d2f7dad016fff4f785b44ab78
ThisCatLikesCrypto <105151352+ThisCatLikesCrypto@users.noreply.github.com>
2024-03-14 19:27:54 +0000

    cokie - uses names that actually make sense, and fix stuff

--- games/cokie/weard.js
+++ games/cokie/weard.js
@@ -1,70 +1,52 @@
-  let cokcl=0
-  let gann=0
-  let curse=0
-  let msg=0
+let cokcl=0
+let cursorAmount=0
+let procursorAmount=0
+let msg=0
+let cookiesGainedByClicking=1
   
-  function red() {
-  document.title = cokcl + " cokies - Cokie cliker";
+function alwaysOn100() {
+  //Update Message Board
+  if (cokcl > 10000){
+    msg="Huge monopoly, you bought out all the cokie businesses and are the only cokie company left"
+  }
+  else if (cokcl > 1000){
+    msg="Massive corporation, the country enjoys your cokies"
+  }
+  else if (cokcl > 100){
+    msg="Local enterprise, you have many happy customers";
+  }
+  else if (cokcl > 10){
+    msg="Beginner business: Supplying stuff for the local female trooper group";
+  }
+  else{
+    msg="Your business just begun, have fun!";
+  }
+  document.getElementById("messages").innerHTML = msg;
 
-    if (cokcl > 10000){
-     msg="Huge monopoly, you bought out all the cokie businesses and are the only cokie company left"
-     document.getElementById("sus").innerHTML = msg
-   }
-   else if (cokcl > 1000){
-     msg="Massive corporation, the county enjoys your cokies"
-     document.getElementById("sus").innerHTML = msg
-   }
-   else if (cokcl > 100){
-     msg="Local enterprise, you have many happy customers"
-     document.getElementById("sus").innerHTML = msg
-    }
-   else if (cokcl > 10){
-      msg="Beginner business: Supplying stuff for the local female trooper group"
-      document.getElementById("sus").innerHTML = msg
-   }
-   else{
-     msg="Your business just begun, have fun!"
-     document.getElementById("sus").innerHTML = msg
-   }
-  }
+  //Update Counter
+  cokcl += 0.05 * cursorAmount;
+  cokcl += 0.1 * procursorAmount;
+  document.getElementById("counter").innerHTML = cokcl;
+  document.title = cokcl + " cokies - Cokie cliker";
+}
+
   
-  setInterval(red, 30);
-  
-  function wot() {
-    cokcl += 1
-    document.getElementById("lol").innerHTML = cokcl
-  }
-  function e(){
-  document.getElementById("lol").innerHTML = cokcl
-  }
-  function idl(){
+function cookieClicked() {
+    cokcl += cookiesGainedByClicking;
+    document.getElementById("counter").innerHTML = cokcl;
+}
+
+function buyProCursor(){
     if (cokcl > 10){
       cokcl-=10
-      gann+=1
-      document.getElementById("lol").innerHTML = cokcl;
+      procursorAmount+=1
     }
-  }
-function ildcurse(){
+}
+function buyCursor(){
   if (cokcl > 5){
-    curse+=1
+    cursorAmount+=1
     cokcl-=5
-    document.getElementById("lol").innerHTML = cokcl;
   }
 }
-function ildgencurse(){
-      cokcl += 0.5 * curse;
-    document.getElementById("lol").innerHTML = cokcl;
-}
 
-setInterval(ildgencurse, 1000);
-
-
-function idlgen() {
-    cokcl += 1 * gann;
-    document.getElementById("lol").innerHTML = cokcl;
-}
-
-setInterval(idlgen, 1000);
-
-
-  
+setInterval(alwaysOn100, 100);
