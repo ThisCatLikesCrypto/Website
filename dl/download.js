84c7778bddbafe775422cbdc06e7da814000f208
ThisCatLikesCrypto <105151352+ThisCatLikesCrypto@users.noreply.github.com>
2024-03-29 16:28:13 +0000

    I made a downloads page
    so basically to add a download, you can use Google Drive and/or GitHub and/or (if the file is small enough) just add it to dl/. Once you've done that add to the if statements in download.js and link it with downloads.html?fileName=[fileName]

---
+++ dl/download.js
@@ -0,0 +1,52 @@
+var gDriveLink;
+var directLink;
+var archiveLink;
+var GitHubLink;
+
+function SetdlOptions(fileName){
+    if (fileName == "EEEEEE.zip"){
+        gDriveLink = "None";
+        directLink = "code/EEEEEE.zip";
+        GitHubLink = "None";
+    } else {
+        document.getElementById('h2FileDisp').innerHTML = "That file doesn't exist."
+    }
+
+    let gdLink = document.getElementById('gDriveLink')
+    let dLink = document.getElementById('directLink')
+    let ghLink = document.getElementById('GitHubLink')
+
+    gdLink.innerHTML = "Google Drive Link: " + gDriveLink;
+    dLink.innerHTML = "Direct Download (Cloudflare CDN) Link: " + directLink;
+    ghLink.innerHTML = "GitHub Link: " + GitHubLink;
+
+    if (gDriveLink == "None"){
+        gdLink.href = "../";
+    } else {
+        gdLink.href = gDriveLink;
+    }
+
+    if (directLink == "None"){
+        dLink.href = "../";
+    } else {
+        dLink.href = directLink;
+    }
+
+    if (GitHubLink == "None"){
+        ghLink.href = "../";
+    } else {
+        ghLink.href = ghLink;
+    }
+
+
+
+
+}
+
+function onload(){
+    const urlParams = new URLSearchParams(window.location.search);
+    const fileName = urlParams.get('fileName');
+    console.log(fileName);
+    document.getElementById('h2FileDisp').innerHTML = "Selected File: " + fileName;
+    SetdlOptions(fileName);
+}
