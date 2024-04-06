037e46b397bc1f4eae045ccb961901454d7f84f3
Wilbur Williams <105151352+ThisCatLikesCrypto@users.noreply.github.com>
2024-04-06 13:57:34 +0100

    fix the text editor dlSave and add upSave
    it also saves every 10 seconds instead of every second because that's unnecessary and a battery hog
    
    do you need any food wilbur

--- textedit/script.js
+++ textedit/script.js
@@ -1,5 +1,4 @@
 var quill = "justfuckingworkplsplspls";
-console.log("page not ded yet :D");
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -17,13 +16,18 @@ function save(quill) {
     return data;
 }
 
-function saveLocal(quill){
-    stuff = save(quill);
-    localStorage.setItem('storedText', stuff);
+function saveLocal(){
+    try {
+        stuff = save(quill);
+        localStorage.setItem('storedText', stuff);
+        console.log("..success!");
+    } catch(error) {
+        console.log("failed to save to local storage. error: " + error);
+    }
 }
 
 function dlSave(){
-    //NO WORKY FIGURE OUT LATER
+    console.log("dlSave requested");
     let textFile = null;
     const makeTextFile = (text) => {
       const data = new Blob ([text], { type: 'text/plain', });
@@ -36,25 +40,65 @@ function dlSave(){
     const link = document.createElement ('a');
     link.setAttribute ('download', "texteditSaveFile");
     link.href = makeTextFile(save(quill));
-    link.click();
+    try {
+        link.click();
+        console.log("successfully downloaded text file");
+    } catch(error) {
+        console.log("fuck you :D " + error);
+    }
+}
+
+function upSave(){
+    console.log("upSave requested");
+    var input = document.createElement('input');
+    input.type = 'file';
+    input.onchange = (e) => {
+      var file = e.target.files[0];
+      var reader = new FileReader();
+      reader.readAsText(file, 'UTF-8');
+      reader.onload = (readerEvent) => {
+        var content = readerEvent.target.result;
+        var data = JSON.parse(content);
+        console.log(data);
+        quill.setContents(data);
+      }
+    }
+    try {
+        input.click();
+        console.log("load success!");
+    } catch(error) {
+        console.log("fuck you :D " + error);
+    }
+
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    var Delta = Quill.import('delta');
-    quill = new Quill('#editor', {
-    modules: {
-      toolbar: '#toolbar'
-    },
-    theme: 'snow'
-    });
-    // Store accumulated changes
 
+    //Only init quill after everything has loaded because otherwise it throws an error :shrug:
+    console.log("initalise quill");
     try {
-    data = JSON.parse(localStorage.getItem('storedText'));
-    quill.setContents(data);
-    console.log(data);
-    } catch {
-    console.log("fuck you! :D")
+        var Delta = Quill.import('delta');
+        quill = new Quill('#editor', {
+        modules: {
+        toolbar: '#toolbar'
+        },
+        theme: 'snow'
+        });
+        console.log("..success!");
+    } catch(error) {
+        console.log("quill broke with " + error)
+        document.getElementById('infoh1').innerHTML = "quill failed to load.";
+        document.getElementById('infop1').innerHTML = "Please refresh the page. If this this issue persists, please create an issue on https://github.com/ThisCatLikesCrypto/Website (not a link)";
+    }
+
+    console.log("load previous save");
+    try {
+        data = JSON.parse(localStorage.getItem('storedText'));
+        quill.setContents(data);
+        console.log("..success!");
+        console.log(data);
+    } catch(error) {
+        console.log("save failed to load. If this is the first load, ignore this. Otherwise create an issue on https://github.com/ThisCatLikesCrypto/Website" + error);
     }
 
     var change = new Delta();
@@ -62,6 +106,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
         change = change.compose(delta);
     });
 
-    setInterval(function() {saveLocal( quill)}, 1000)
+    setInterval(saveLocal, 10000)
     setInterval(countChars, 500);
 });
