3f1529057a0caad52f4140f289a896fe405f8865
Pittab <120305267+Pittab@users.noreply.github.com>
2024-04-10 18:34:54 +0100

    added stronge version 0.4

--- dl/download.js
+++ dl/download.js
@@ -1,68 +1,106 @@
 var fileObject = "";
 var data = {};
 
-function SetdlOptions(inFileName){
-    data = { //Keep this up to date with files
-        "files":[
-           {"fileName": "EEEEEE.zip", "gDriveLink": "None", "directLink": "code/EEEEEE.zip", "GitHubLink": "None"},
-           {"fileName": "infiwriteRust.zip", "gDriveLink": "None", "directLink": "compiledprograms/infiwriteRust.zip", "GitHubLink": "None"},
-           {"fileName": "ESDlang.zip", "gDriveLink": "None", "directLink": "es/ESDlang.zip", "GitHubLink": "None"},
-           {"fileName": "Stronge_v0.1.zip", "gDriveLink": "None", "directLink": "es/Stronge_v0.1.zip", "GitHubLink": "None"},
-           {"fileName": "Stronge_v0.2.zip", "gDriveLink": "None", "directLink": "es/Stronge_v0.2.zip", "GitHubLink": "None"},
-           {"fileName": "Stronge_v0.3.zip", "gDriveLink": "None", "directLink": "es/Stronge_v0.3.zip", "GitHubLink": "None"}
-        ]   
-    };
+function SetdlOptions(inFileName) {
+  data = {
+    //Keep this up to date with files
+    files: [
+      {
+        fileName: "EEEEEE.zip",
+        gDriveLink: "None",
+        directLink: "code/EEEEEE.zip",
+        GitHubLink: "None",
+      },
+      {
+        fileName: "infiwriteRust.zip",
+        gDriveLink: "None",
+        directLink: "compiledprograms/infiwriteRust.zip",
+        GitHubLink: "None",
+      },
+      {
+        fileName: "ESDlang.zip",
+        gDriveLink: "None",
+        directLink: "es/ESDlang.zip",
+        GitHubLink: "None",
+      },
+      {
+        fileName: "Stronge_v0.1.zip",
+        gDriveLink: "None",
+        directLink: "es/Stronge_v0.1.zip",
+        GitHubLink: "None",
+      },
+      {
+        fileName: "Stronge_v0.2.zip",
+        gDriveLink: "None",
+        directLink: "es/Stronge_v0.2.zip",
+        GitHubLink: "None",
+      },
+      {
+        fileName: "Stronge_v0.3.zip",
+        gDriveLink: "None",
+        directLink: "es/Stronge_v0.3.zip",
+        GitHubLink: "None",
+      },
+      {
+        fileName: "Stronge_v0.4.zip",
+        gDriveLink: "None",
+        directLink: "es/Stronge_v0.4.zip",
+        GitHubLink: "None",
+      },
+    ],
+  };
 
-    try {
-        fileObject = data.files.find(file => file.fileName === inFileName);
-    } catch {
-        document.getElementById('h2FileDisp').innerHTML = "That File Doesn't Exist.";
-        console.log(data);
-    }
-    
-    // Extract the relevant information
-    const fileName = fileObject.fileName;
-    const gDriveLink = fileObject.gDriveLink;
-    const directLink = fileObject.directLink;
-    const GitHubLink = fileObject.GitHubLink;
+  try {
+    fileObject = data.files.find((file) => file.fileName === inFileName);
+  } catch {
+    document.getElementById("h2FileDisp").innerHTML =
+      "That File Doesn't Exist.";
+    console.log(data);
+  }
 
-    console.log("File Name:", fileName);
-    console.log("Google Drive Link:", gDriveLink);
-    console.log("Direct Link:", directLink);
-    console.log("GitHub Link:", GitHubLink);
+  // Extract the relevant information
+  const fileName = fileObject.fileName;
+  const gDriveLink = fileObject.gDriveLink;
+  const directLink = fileObject.directLink;
+  const GitHubLink = fileObject.GitHubLink;
 
-    let gdLink = document.getElementById('gDriveLink')
-    let dLink = document.getElementById('directLink')
-    let ghLink = document.getElementById('GitHubLink')
+  console.log("File Name:", fileName);
+  console.log("Google Drive Link:", gDriveLink);
+  console.log("Direct Link:", directLink);
+  console.log("GitHub Link:", GitHubLink);
 
-    gdLink.innerHTML = "Google Drive Link: " + gDriveLink;
-    dLink.innerHTML = "Direct Download (Cloudflare CDN) Link: " + directLink;
-    ghLink.innerHTML = "GitHub Link: " + GitHubLink;
+  let gdLink = document.getElementById("gDriveLink");
+  let dLink = document.getElementById("directLink");
+  let ghLink = document.getElementById("GitHubLink");
 
-    if (gDriveLink == "None"){
-        gdLink.href = "../";
-    } else {
-        gdLink.href = gDriveLink;
-    }
+  gdLink.innerHTML = "Google Drive Link: " + gDriveLink;
+  dLink.innerHTML = "Direct Download (Cloudflare CDN) Link: " + directLink;
+  ghLink.innerHTML = "GitHub Link: " + GitHubLink;
 
-    if (directLink == "None"){
-        dLink.href = "../";
-    } else {
-        dLink.href = directLink;
-    }
+  if (gDriveLink == "None") {
+    gdLink.href = "../";
+  } else {
+    gdLink.href = gDriveLink;
+  }
 
-    if (GitHubLink == "None"){
-        ghLink.href = "../";
-    } else {
-        ghLink.href = ghLink;
-    }
+  if (directLink == "None") {
+    dLink.href = "../";
+  } else {
+    dLink.href = directLink;
+  }
 
+  if (GitHubLink == "None") {
+    ghLink.href = "../";
+  } else {
+    ghLink.href = ghLink;
+  }
 }
 
-function onload(){
-    const urlParams = new URLSearchParams(window.location.search);
-    const fileName = urlParams.get('fileName');
-    console.log(fileName);
-    document.getElementById('h2FileDisp').innerHTML = "Selected File: " + fileName;
-    SetdlOptions(fileName);
+function onload() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const fileName = urlParams.get("fileName");
+  console.log(fileName);
+  document.getElementById("h2FileDisp").innerHTML =
+    "Selected File: " + fileName;
+  SetdlOptions(fileName);
 }
