70a6a2f346c07248b85c4f69d2340bf4833abae2
Pittab <120305267+Pittab@users.noreply.github.com>
2024-04-10 09:28:35 +0100

    optimised sub optimal code

--- pittab/innovation.js
+++ pittab/innovation.js
@@ -1,11 +1,10 @@
 let ino = 0;
 
-function innovation() {
-  ino += 1;
-  document.getElementById("inocount").innerHTML = ino + " innovations";
-}
-
-function unin() {
-  ino -= 1;
+function innovation(string) {
+  if (string == "True") {
+    ino += 1;
+  } else {
+    ino -= 1;
+  }
   document.getElementById("inocount").innerHTML = ino + " innovations";
 }
