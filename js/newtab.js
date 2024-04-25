95d0ac127356f3e5a151f25ac392d4e63d11e833
ThisCatLikesCrypto <105151352+ThisCatLikesCrypto@users.noreply.github.com>
2024-04-25 17:56:06 +0100

    begin work on cool looking newtab

---
+++ js/newtab.js
@@ -0,0 +1,14 @@
+function searchEcosia() {
+    var searchTerm = document.getElementById("searchInput").value;
+    window.location.href = "https://www.ecosia.org/search?q=" + encodeURIComponent(searchTerm);
+    return false; // Prevent default form submission
+}
+
+function searchGoogle() {
+    var searchTerm = document.getElementById("searchInput").value;
+    window.location.href = "https://google.com/search?q=" + encodeURIComponent(searchTerm);
+}
+
+function goPlaces(place){
+    window.location.href=place;
+}
