08b72848ad721a470f93b82d13ffd9128e4a158e
ThisCatLikesCrypto <105151352+ThisCatLikesCrypto@users.noreply.github.com>
2024-04-25 18:36:20 +0100

    added gclass and changer
    hopefully saves to a cookie

--- js/newtab.js
+++ js/newtab.js
@@ -1,3 +1,37 @@
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+var classroomLink = getCookie("classroomLink") || 'https://classroom.google.com'; // Default classroom link
+
+function changeClassroomLink(link) {
+    classroomLink = link;
+    setCookie("classroomLink", link, 30); // Save the choice in a cookie for 30 days
+}
+
+function getClassroomLink() {
+    return classroomLink;
+}
+
 function searchEcosia() {
     var searchTerm = document.getElementById("searchInput").value;
     if (searchTerm === ""){
