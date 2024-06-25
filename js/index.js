cfeaaea4b3578e2ab0896e6873821563b532b2d4
ThisCatLikesCrypto <105151352+ThisCatLikesCrypto@users.noreply.github.com>
2024-06-25 19:43:53 +0100

    SPLASH TEXT YEEEYA

--- js/index.js
+++ js/index.js
@@ -1,4 +1,13 @@
 var eaudion = false;
+var splashtext = ["Why does this exist again", "prefers-reduced-motion: no movement", 
+  "hmmm, what to put here?", "defying gravity", "HTML2 is just an archive", "Wine Is Not an Emulator",
+  "built with VSCode", "<a href='https://www.youtube.com/watch?v=M93mt3NzkmM'>This song has no right being this good</a>",
+  "<a href='https://www.youtube.com/watch?v=24sx3aFynQI'>1996 stock motors</a>"];
+
+function choose(choices) {
+  var index = Math.floor(Math.random() * choices.length);
+  return choices[index];
+}
 
 function getUKTime() {
     // Create a Date object for the current date and time
@@ -32,6 +41,9 @@ function getUKTime() {
 function playmusic(){
     if (!eaudion){
       audio.play();
+      muct = document.getElementById('musiccontrol');
+      muct.innerHTML = "Click to pause music";
+      muct.onclick = pausemusic;
       audio.addEventListener("ended", function(){
         audio.currentTime = 0;
         audio.play();
@@ -39,8 +51,17 @@ function playmusic(){
     }
   }
 
+function pausemusic(){
+  audio.pause();
+  eaudion = false;
+  muct = document.getElementById('musiccontrol');
+  muct.innerHTML = "Click to resume music";
+  muct.onclick = playmusic;
+}
+
 document.addEventListener('DOMContentLoaded', async function(){
     document.getElementById('mytime').innerHTML = getUKTime();
+    document.getElementById('splash').innerHTML = choose(splashtext);
 });
 
 setInterval(function(){
