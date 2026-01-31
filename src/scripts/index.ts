42fe9146de95fa124166541065d871c483fdf35f
ThisCatLikesCrypto <105151352+ThisCatLikesCrypto@users.noreply.github.com>
2026-01-31 12:33:35 +0000

    im gonna fly some planes (minecraft villager version)

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -82,9 +82,7 @@ const splashtext = [
     "built with VSCode",
     "<a href='https://www.youtube.com/watch?v=M93mt3NzkmM'>This song has no right being this good</a>",
     "<a href='https://www.youtube.com/watch?v=24sx3aFynQI'>1996 stock motors</a>",
-    "<img style='height: 32px; width: 32px;' src='https://assets.c48.uk/spinny_cat/spinny_cat_ace.avif' alt='spinny_cat_ace'>",
     "<img style='height: 32px; width: 32px;' src='https://assets.c48.uk/spinny_cat/spinny_cat_aroace.avif' alt='spinny_cat_aroace'>",
-    "<img style='height: 32px; width: 32px;' src='https://assets.c48.uk/spinny_cat/spinny_cat_aro.avif' alt='spinny_cat_aro'>",
     "<a href='https://www.youtube.com/watch?v=83CqPViJB0I'>UNIFY</a>",
     "Killer Queen"
 ];
